ea3ab5979d23c7f53195993cf51acfb3e1af1a8d
Gaston <64218013+Gasthor@users.noreply.github.com>
2024-05-24 18:20:26 -0400

    [FIX] CORS configuration issues and load environment variables from .env file

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
 from routes.files import files_bp
 
+load_dotenv()
+
+origins = os.environ.get("ACCESS_CONTROL_ALLOW_ORIGIN", "").split(",")
+print("CORS origins:", origins)
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+# Configurar CORS para manejar orígenes explícitamente
+CORS(app, origins=origins)
 app.register_blueprint(files_bp, url_prefix='/api/files')
+
 
 @app.route('/', methods=['GET'])
 def health():
     return "Server is Up and Running"
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
